3b7de068b8afbb7cf1c648d2437f065c7862617b
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-02-22 19:48:38 +0530

    fifth

--- avasah/settings.py
+++ avasah/settings.py
@@ -118,7 +118,7 @@ import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 
 
 # Default primary key field type
