285e05409725b167e3c8afbae88a3231683a8a85
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-02-22 19:38:05 +0530

    second

--- info/build_files.sh
+++ info/build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+echo " BUILD START"
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
+echo " BUILD ENDS"
