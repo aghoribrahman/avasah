e163957c02068d0291607a9d545e4c96ba2ce12d
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-02-22 19:56:38 +0530

    seven

--- avasah/settings.py
+++ avasah/settings.py
@@ -116,9 +116,9 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'lib', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
